010615b430b93c7a5dab57944c3d0ddf5d39ec99
zhuangyusong <zhangyusong@lattebank.com>
2019-12-17 16:37:03 +0800

    添加获取资方年利率的方法

--- src/test/java/tk/mybatis/springboot/test/CountryServiceTest.java
+++ src/test/java/tk/mybatis/springboot/test/CountryServiceTest.java
@@ -1,5 +1,8 @@
 package tk.mybatis.springboot.test;
 
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSONPath;
+import org.json.JSONException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +11,9 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 import tk.mybatis.springboot.Application;
 import tk.mybatis.springboot.mapper.BfProcessInfoMapper;
+import tk.mybatis.springboot.mapper.CreditInfoMapper;
 import tk.mybatis.springboot.mapper.TestMapper;
+
 
 /**
  * @author liuzh
@@ -23,11 +28,35 @@ public class CountryServiceTest {
     private TestMapper testMapper;
 
     @Autowired
+    private CreditInfoMapper creditInfoMapper;
+
+    @Autowired
     private BfProcessInfoMapper bfProcessInfoMapper;
 
     @Test
     public void test() {
         int i = bfProcessInfoMapper.selectData();
         System.err.println("成功查询到"+i+"条");
+    }
+
+    @Test
+    public void test02() throws JSONException {
+        String responseInfo = creditInfoMapper.selectCreditInfo("5174e809-252c-45b1-843d-e596fcfd2069", "PSBCXJ");
+//        System.err.println("数据库原始输出："+psbcxj);
+//        JSONObject jsonObject = new JSONObject(psbcxj);
+//        System.err.println("转为json对象："+jsonObject.get("bank"));
+//        JSONObject jsonObject2 = new JSONObject(jsonObject.get("respJson").toString());
+//        System.out.println("获取年利率的值列表："+jsonObject2.get("rateList"));
+//        JSONObject jsonObject3 = new JSONObject(jsonObject2.get("rateList").toString());
+//        System.out.println("获取年利率的值："+jsonObject3.get("12"));
+        String annualInterestRate = com.alibaba.fastjson.JSONObject.parseObject(com.alibaba.fastjson.JSONObject.parseObject(JSONObject.parseObject(responseInfo).get("respJson").toString()).get("rateList").toString()).get("12").toString();
+        System.out.println("获取年利率的值："+annualInterestRate);
+
+
+//        把json字符串转为json对象
+//        JSONObject jsonObject = JSONObject.parseObject(jsonString);
+//        Object respJson = JSONObject.parseObject(jsonString).get("respJson");
+//        String string = JSONObject.toJSONString(respJson);
+//        System.err.println("成功查询到psbcxj"+jsonObject.get("respJson"));
     }
 }
